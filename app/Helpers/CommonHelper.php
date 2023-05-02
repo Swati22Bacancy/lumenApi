8ca280e454ec3a69813381f19d9f67d0cc9d00fb
Swati22Bacancy <67266963+Swati22Bacancy@users.noreply.github.com>
2023-05-02 15:56:56 +0530

    generate ramdom string function

--- app/Helpers/CommonHelper.php
+++ app/Helpers/CommonHelper.php
@@ -8,4 +8,14 @@ class CommonHelper
     {
         return $date;
     }
+    function generateRandomString($length = 10)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
 }
